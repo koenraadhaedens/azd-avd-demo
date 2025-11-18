5652c7e28cac738f66a78fde7a8e69712daac1d0
Koenraad HAadens <Koenraad@khd.be>
2025-11-18 11:32:19 +0000

    change post script

--- scripts/sessionhost/ConfigureSessionHost.ps1
+++ scripts/sessionhost/ConfigureSessionHost.ps1
@@ -59,7 +59,6 @@ try {
             Write-Error "Domain join has not completed. Continuing with configuration..."
         }
     }
-}
 catch {
     Write-Error "Failed to verify domain join status: $($_.Exception.Message)"
 }
